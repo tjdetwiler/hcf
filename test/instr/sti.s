4e8c09134c00ff16982ccda83fb140d846545017
Tim Detwiler <timdetwiler@gmail.com>
2012-06-11 22:58:01 -0400

    Add some unit tests.
    Add unit test for xor and sti. Both pass as of this revision.

--- test/instr/sti.s
+++ test/instr/sti.s
@@ -1,4 +1,20 @@
 ; 'sti' unit test
-;! fail("sti unit test is unimplemented");
+
+; Basic Test
+set i, 5
+set j, 3
+sti a, 3
+;! assert(cpu.regI() == 6);
+;! assert(cpu.regJ() == 4);
+;! assert(cpu.regA() == 3);
+
+; Overflow Test
+set i, 0xffff
+set j, 0xffff
+sti a, 34
+;! assert(cpu.regI() == 0);
+;! assert(cpu.regJ() == 0);
+;! assert(cpu.regA() == 34);
+;! pass(); 
 
 :crash set pc, crash
--- test/instr/xor.s
+++ test/instr/xor.s
@@ -1,4 +1,17 @@
 ; 'xor' unit test
-;! fail("xor unit test is unimplemented");
+
+; Basic tests
+set a, 0xffff
+set b, 0xffff
+xor b, a
+;! assert(cpu.regA() == 0xffff);
+;! assert(cpu.regB() == 0x0);
+
+set a, 0xf0f0
+set b, 0xffff
+xor b, a
+;! assert(cpu.regA() == 0xf0f0);
+;! assert(cpu.regB() == 0x0f0f);
+;! pass();
 
 :crash set pc, crash
