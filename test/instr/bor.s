9445e1b69ced7ef4664071ff89f31f214d469c03
vikki <vikkic@vt.edu>
2012-06-12 21:54:57 -0400

    added adx, sbx, and asr, along with some tests

--- test/instr/bor.s
+++ test/instr/bor.s
@@ -1,4 +1,16 @@
 ; 'bor' unit test
-;! fail("bor unit test is unimplemented");
+
+set a, 0x5555
+set b, 0xaaaa
+bor a, b
+;! assert(cpu.regA() == 0xffff)
+;! assert(cpu.regB() == 0xaaaa)
+
+set a, 0x3381
+set b, 0x5527
+bor a, b
+;! assert(cpu.regA() == 0x77a7)
+;! assert(cpu.regB() == 0x5527)
+;! pass()
 
 :crash set pc, crash
